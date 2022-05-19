76881f031efc09c33e8ae7f3bd7db66c753abfd4
Jean-Luc Aubert <jean-luc.aubert@aelion.fr>
2022-05-19 13:31:47 +0200

    Add mysql dependency and connection

---
+++ src/com/aelion/database/MySQLConnect.java
@@ -0,0 +1,52 @@
+package com.aelion.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Singleton de connexion à la base de données
+ * -	Constructeur privé
+ * -	Méthode statique pour récupérer une instance
+ * 
+ * @author Aelion
+ *
+ */
+public class MySQLConnect {
+	/**
+	 * Stocke de manière statique l'instance de connexion au serveur
+	 */
+	private static Connection instance = null;
+	
+	private MySQLConnect() {
+
+		try {
+			MySQLConnect.instance = DriverManager.getConnection(  
+					"jdbc:mysql://127.0.0.1:3306/players", 
+					"root", 
+					"root"
+			);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Access denied for : root on 127.0.0.1 " + e.getMessage());
+		}  
+	}
+	
+	public static Connection getInstance() {
+		if (MySQLConnect.instance == null) {
+			new MySQLConnect();
+		}
+		
+		return MySQLConnect.instance;
+	}
+	
+	public static void close() {
+		try {
+			MySQLConnect.instance.close();
+			MySQLConnect.instance = null;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+}
